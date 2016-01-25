6d29159930b19f7709ba6de881b8c5390cb4c9fd
RobinBKing <samimspop@gmail.com>
2016-01-24 19:35:12 -0800

    added form html and input code.

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,12 @@
 describe('Contact', function() {
-  it("Creates a new contact with given properties", function() {
+  it("creates a new contact with given properties", function() {
     var testContact = new Contact("John","Doe");
     expect(testContact.firstName).to.equal("John");
     expect(testContact.lastName).to.equal("Doe");
     expect(testContact.addresses).to.eql([]);
+  });
+  it("adds the fullName mathod to all contacts", function() {
+    var testContact = new Contact("Jesse", "James");
+    expect(testContact.fullName()).to.equal("Jesse James");
   });
 });
